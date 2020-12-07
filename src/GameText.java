151703b8f7a787b0519388568d7e1ad81cabfc06
Lucas Benning <benning.luke@gmail.com>
2020-12-07 11:34:21 -0500

    Change project structure

--- src/GameText.java
+++ src/GameText.java
@@ -1,3 +1,5 @@
+package src;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -10,7 +12,7 @@ public class GameText {
   private int step;
   private List<String> lines;
   private Map<Integer, Integer> currentChoices;
-  
+
   public GameText() {
     this.step = 1;
     this.lines = new ArrayList<String>();
@@ -20,20 +22,30 @@ public class GameText {
 
   public void getCurrentText() {
     currentChoices.clear();
-    for(int i = step; i < lines.size(); i++) {
+    for (int i = step; i < lines.size(); i++) {
       // if it is a comment
-      if(lines.get(i).charAt(0) != '#') {
+      if (lines.get(i).charAt(0) != '#') {
         // check for end of block
-        if(!lines.get(i).equals("---") && !lines.get(i).equals("???")) {
+        if (!lines.get(i).equals("---") && !lines.get(i).equals("???")
+          && !lines.get(i).contains("SLEEP")) {
           System.out.println(lines.get(i));
         }
         // choices
         if (lines.get(i).equals("???")) {
           // store choices in map
           int j = i + 1;
-          while(!lines.get(j).equals("---")) {
-            currentChoices.put(j-i, j);
+          while (!lines.get(j).equals("---")) {
+            currentChoices.put(j - i, j);
             j++;
+          }
+        }
+        /// pause text display
+        if (lines.get(i).contains("SLEEP")) {
+          int sleepDuration = Integer.parseInt(lines.get(i).substring(lines.get(i).indexOf(" ") + 1));
+          try {
+            Thread.sleep(sleepDuration * 1000);
+          } catch (InterruptedException e) {
+            e.printStackTrace();
           }
         }
         // end of block
